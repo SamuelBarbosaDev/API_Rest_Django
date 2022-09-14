56e412a0d9f6779bcd65489a2f4f7d2df86e0af1
SamuelBarbosaDev <samueloficial@protonmail.com>
2022-09-14 15:00:22 -0300

    alteração no admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from core.models import *
 
-# Register your models here.
+
+# Cliente.
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    list_filter= ('nome',)
+    search_fields = ['nome']
+
+
+    class Meta:
+        model = Cliente
