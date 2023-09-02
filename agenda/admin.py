7be9e930e12b5d25871caf766bd37093a0f4e5f4
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-01 22:32:45 -0300

    feat: Adicionando o model Agendamento a área de admin

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from agenda.models import Agendamento
 
-# Register your models here.
+
+class AgendamentoAdmin(admin.ModelAdmin):
+    list_display = ('data_horario', 'nome_cliente', 'email_cliente', 'telefone_cliente')
+
+admin.site.register(Agendamento, AgendamentoAdmin)
