8fdec7fceccbe3165afca214cdb886e040bcfc1b
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-01 22:49:42 -0300

    feat: Adicionando a view agendamento_list

--- agenda/views.py
+++ agenda/views.py
@@ -4,8 +4,15 @@ from rest_framework.response import Response
 from agenda.models import Agendamento
 from django.http import JsonResponse
 
+
 def agendamento_detail(request, id):
     obj = get_object_or_404(Agendamento, id=id)
     serializer = AgendamentoSerializer(obj)
 
     return JsonResponse(serializer.data)
+
+
+def agendamento_list(request):
+    qs = Agendamento.objects.all()
+    serializer = AgendamentoSerializer(qs, many=True)
+    return JsonResponse(serializer.data, safe=False)
