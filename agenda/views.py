f29b2b0834653be01db7dc5000cb7bd5ef3dcba1
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-01 22:33:48 -0300

    feat: Adicionando a view agendamento_detail

--- agenda/views.py
+++ agenda/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from agenda.serializers import AgendamentoSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from agenda.models import Agendamento
+from django.http import JsonResponse
 
-# Create your views here.
+def agendamento_detail(request, id):
+    obj = get_object_or_404(Agendamento, id=id)
+    serializer = AgendamentoSerializer(obj)
+
+    return JsonResponse(serializer.data)
