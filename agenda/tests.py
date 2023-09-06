6b439007d37f8c359af200c249065aab899daa08
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-06 12:02:48 -0300

    test: Verificando os dados retornado pela api  - Criei o teste 'test_listagem_vazia' para testa o método GET da api '/api/agendamento_list'  - Criei o teste 'test_listagem_de_agendamentos_criados' para testa o método POST da api '/api/agendamento_list'

--- agenda/tests.py
+++ agenda/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+import json
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class TestListagemAgendamentos(APITestCase):
+    def test_listagem_vazia(self):
+        response = self.client.get('/api/agendamento_list')
+        data = json.loads(response.content)
+        self.assertEqual(data, [])
+
+    def test_listagem_de_agendamentos_criados(self):
+        data = {
+            "data_horario": "2023-09-02T05:08:10Z",
+            "nome_cliente": "Luana52",
+            "email_cliente": "Luana52@Luana52.com",
+            "telefone_cliente": 123456789012
+        }
+
+        response = self.client.post(
+            path='/api/agendamento_list',
+            data=data,
+            format='json',
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
