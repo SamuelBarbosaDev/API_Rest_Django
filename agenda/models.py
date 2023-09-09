3017de611abca85c88ceeb6b82671cc4eab6fda8
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-09 10:56:05 -0300

    feat: Adicionando o campo cancelamento_cliente ao model Agendamento

--- agenda/models.py
+++ agenda/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-# Create your models here.
 
 class Agendamento(models.Model):
     data_horario = models.DateTimeField()
     nome_cliente = models.CharField(max_length=43)
     email_cliente = models.EmailField()
     telefone_cliente = models.CharField(max_length=13)
-    
+    cancelamento_cliente = models.BooleanField(default=False)
