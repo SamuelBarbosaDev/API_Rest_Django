5a6eee8c90cbcacb5138cfdd31f548a8d669f063
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-10 07:46:30 -0300

    refactor: Refatorando o agenda/serializers.py adicionando o ModelSerializer

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -3,11 +3,15 @@ from rest_framework import serializers
 from agenda.models import Agendamento
 
 
-class AgendamentoSerializer(serializers.Serializer):
-    data_horario = serializers.DateTimeField()  # YYYY-MM-DDThh:mm
-    nome_cliente = serializers.CharField(max_length=43)
-    email_cliente = serializers.EmailField()
-    telefone_cliente = serializers.CharField(max_length=13)
+class AgendamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agendamento
+        fields = [
+            'data_horario',
+            'nome_cliente',
+            'email_cliente',
+            'telefone_cliente',
+        ]
 
     def validate_data_horario(self, value):
         if value < timezone.now():
@@ -32,33 +36,3 @@ class AgendamentoSerializer(serializers.Serializer):
             )
 
         return attrs
-
-    def create(self, validated_data):
-        agendamento = Agendamento.objects.create(
-            data_horario=validated_data['data_horario'],
-            nome_cliente=validated_data['nome_cliente'],
-            email_cliente=validated_data['email_cliente'],
-            telefone_cliente=validated_data['telefone_cliente'],
-        )
-
-        return agendamento
-
-    def update(self, instance, validated_data):
-        instance.data_horario = validated_data.get(
-            'data_horario',
-            instance.data_horario
-        )
-        instance.nome_cliente = validated_data.get(
-            'nome_cliente',
-            instance.nome_cliente
-        )
-        instance.email_cliente = validated_data.get(
-            'email_cliente',
-            instance.email_cliente
-        )
-        instance.telefone_cliente = validated_data.get(
-            'telefone_cliente',
-            instance.telefone_cliente
-        )
-        instance.save()
-        return instance
