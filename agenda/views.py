42250d4f09b84d1204df31037c89e70c1a74747e
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-11 09:23:49 -0300

    refactor: Adicionando generics views em agenda/views.py

--- agenda/views.py
+++ agenda/views.py
@@ -1,65 +1,13 @@
-from rest_framework import mixins
 from rest_framework import generics
 from agenda.models import Agendamento
 from agenda.serializers import AgendamentoSerializer
 
 
-class AgendamentoDetail(
-    mixins.RetrieveModelMixin,  # GET
-    mixins.UpdateModelMixin,  # PATCH e PUT
-    mixins.DestroyModelMixin,  # DELETE
-    generics.GenericAPIView,  # API genérica
-):
+class AgendamentoList(generics.ListCreateAPIView):
     queryset = Agendamento.objects.all()
     serializer_class = AgendamentoSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(
-            request=request,
-            args=args,
-            kwargs=kwargs,
-        )
 
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(
-            request=request,
-            args=args,
-            kwargs=kwargs,
-        )
-
-    def put(self, request, *args, **kwargs):
-        return self.update(
-            request=request,
-            args=args,
-            kwargs=kwargs,
-        )
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(
-            request=request,
-            args=args,
-            kwargs=kwargs,
-        )
-
-
-class AgendamentoList(
-    mixins.ListModelMixin,  # Adicionando mixin de listagem
-    mixins.CreateModelMixin,  # Adicionando mixin de Criação
-    generics.GenericAPIView,  # Class genérica
-):
+class AgendamentoDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Agendamento.objects.all()
     serializer_class = AgendamentoSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(
-            request=request,
-            args=args,
-            kwargs=kwargs
-        )
-
-    def post(self, request, *args, **kwargs):
-        return self.create(
-            request=request,
-            args=args,
-            kwargs=kwargs
-        )
