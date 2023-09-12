0ec8b0b23d303492a5ef1b50b6dfc06d7514f8aa
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-12 09:21:18 -0300

    feat: SubEscrevendo a função get_queryset na view AgendamentoList em agenda/views.py

--- agenda/views.py
+++ agenda/views.py
@@ -3,9 +3,14 @@ from agenda.models import Agendamento
 from agenda.serializers import AgendamentoSerializer
 
 
+# /api/agendamento_list/?username=admin
 class AgendamentoList(generics.ListCreateAPIView):
-    queryset = Agendamento.objects.all()
     serializer_class = AgendamentoSerializer
+
+    def get_queryset(self):
+        username = self.request.query_params.get('username', None)
+        queryset = Agendamento.objects.filter(prestador__username=username)
+        return queryset
 
 
 class AgendamentoDetail(generics.RetrieveUpdateDestroyAPIView):
