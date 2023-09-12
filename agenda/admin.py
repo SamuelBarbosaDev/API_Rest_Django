8156461cc7531baf2a16cf2e398428e2c79b19d5
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-12 09:18:34 -0300

    feat: Adicionando o campo prestador a list_display em agenda/admin.py

--- agenda/admin.py
+++ agenda/admin.py
@@ -5,6 +5,7 @@ from agenda.models import Agendamento
 class AgendamentoAdmin(admin.ModelAdmin):
     list_display = (
         'data_horario',
+        'prestador',
         'nome_cliente',
         'email_cliente',
         'telefone_cliente',
