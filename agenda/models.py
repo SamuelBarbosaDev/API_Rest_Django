292662f66a0cc9afbf01770635547b53456f1583
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-12 09:17:36 -0300

    feat: Adicionando o campo prestador ao model Agendamento em agenda/models.py

--- agenda/models.py
+++ agenda/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Agendamento(models.Model):
+    prestador = models.ForeignKey(
+        'auth.user',
+        related_name='agendamentos',
+        on_delete=models.CASCADE
+    )
     data_horario = models.DateTimeField()
     nome_cliente = models.CharField(max_length=43)
     email_cliente = models.EmailField()
