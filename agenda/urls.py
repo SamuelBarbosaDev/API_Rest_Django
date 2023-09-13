d257802598310a23126d0c38cd04ad7a8fb26a00
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-13 10:42:03 -0300

    feat: Adicionando "prestador_list/" em agenda/urls.py

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from agenda.views import (
     AgendamentoDetail,
-    AgendamentoList
+    AgendamentoList,
+    PrestadorList,
 )
 
 urlpatterns = [
     path('agendamento_list/', AgendamentoList.as_view()),
-    path('agendamento/<int:pk>/', AgendamentoDetail.as_view())
+    path('agendamento/<int:pk>/', AgendamentoDetail.as_view()),
+    path('prestador_list/', PrestadorList.as_view()),
 ]
