3f132ef0bc7f72bda34804f0ee2203ae90ef90d7
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-13 09:04:35 -0300

    feat: Adicionando a permissão IsAuthenticatedOrReadOnly em agenda/views.py

--- agenda/views.py
+++ agenda/views.py
@@ -1,11 +1,13 @@
 from rest_framework import generics
 from agenda.models import Agendamento
+from rest_framework import permissions
 from agenda.serializers import AgendamentoSerializer
 
 
 # /api/agendamento_list/?username=admin
 class AgendamentoList(generics.ListCreateAPIView):
     serializer_class = AgendamentoSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         username = self.request.query_params.get('username', None)
