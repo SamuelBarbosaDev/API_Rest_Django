6c9a374d24ff4e477e0698a64e64a0f21b4c8b09
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-13 09:34:43 -0300

    feat: Adicionando a permissão "IsOwnerOrCreateOnly" em agenda/views.py

--- agenda/views.py
+++ agenda/views.py
@@ -4,10 +4,24 @@ from rest_framework import permissions
 from agenda.serializers import AgendamentoSerializer
 
 
+class IsOwnerOrCreateOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return True
+
+        username = request.query_params.get('username', None)
+
+        if request.user.username == username:
+            return True
+
+        else:
+            return False
+
+
 # /api/agendamento_list/?username=admin
 class AgendamentoList(generics.ListCreateAPIView):
     serializer_class = AgendamentoSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerOrCreateOnly]
 
     def get_queryset(self):
         username = self.request.query_params.get('username', None)
