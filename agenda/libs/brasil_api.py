d483d4332ad0d3b2e5a796e4ded6607433b12ab5
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-14 09:49:18 -0300

    feat: Adicionando agenda/libs para consumi aplicações de terceiros
     - Consumindo api do brasil api em agenda/libs/brasil_api.py

---
+++ agenda/libs/brasil_api.py
@@ -0,0 +1,27 @@
+import requests
+from datetime import date
+
+
+def is_feriado(data: date) -> bool:
+    """
+    - Chama a APi Com o ano da data.
+    - Verificar se os feriados retornados possuem a data igual a data
+    solicitada pelo nosso usuário.
+    - Caso afirmativo: retorna uma lista vazia.
+    """
+    r = requests.get(
+        url=f'https://brasilapi.com.br/api/feriados/v1/{data.year}'
+    )
+
+    if r.status_code != 200:
+        raise ValueError('Não foi possível consultar os feriados!')
+
+    feriados = r.json()
+
+    for feriado in feriados:
+        data_feriado_as_str = feriado['date']
+        data_feriado = date.fromisoformat(data_feriado_as_str)
+        if data == data_feriado:
+            return True
+
+    return False
