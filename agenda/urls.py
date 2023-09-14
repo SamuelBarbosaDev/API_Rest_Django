bf6bfb6c691b898691b65954c43f43c67e87a279
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-14 09:55:58 -0300

    feat: Adicionando a rota "get_horarios/" em agenda/urls.py

--- agenda/urls.py
+++ agenda/urls.py
@@ -3,10 +3,12 @@ from agenda.views import (
     AgendamentoDetail,
     AgendamentoList,
     PrestadorList,
+    get_horarios
 )
 
 urlpatterns = [
     path('agendamento_list/', AgendamentoList.as_view()),
     path('agendamento/<int:pk>/', AgendamentoDetail.as_view()),
     path('prestador_list/', PrestadorList.as_view()),
+    path('get_horarios/', get_horarios),
 ]
