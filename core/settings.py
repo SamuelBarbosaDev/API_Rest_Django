2a0ba879b021202e0f464095e8ade7c2adc7df6a
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-20 20:36:17 -0300

    feat: Adicionando a variável EMAIL_BACKEND em core/settings.py

--- core/settings.py
+++ core/settings.py
@@ -161,3 +161,5 @@ LOGGING = {  # DictConfig schema: https://docs.python.org/3/library/logging.conf
 
 CELERY_BROKER_URL = config("REDIS_URL", default="redis://localhost:6379/0")
 CELERY_RESULT_BACKEND = config("REDIS_URL", default="redis://localhost:6379/0")
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
