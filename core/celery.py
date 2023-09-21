8224c2c84a2def1291642f830ba71c141c903271
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-21 08:18:13 -0300

    refactor: Refatorando core/celery.py

--- core/celery.py
+++ core/celery.py
@@ -1,17 +1,11 @@
 import os
 from celery import Celery
-from core import settings
-# from decouple import config
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-# config('DJANGO_SETTINGS_MODULE', default='core.settings')
 
 # Cria uma instância do Celery
-app = Celery(
-    'core',
-    broker=settings.CELERY_BROKER_URL,
-    # backend=settings.CELERY_RESULT_BACKEND,  # Descomente esta linha se desejar um backend
-)
+app = Celery('core',)
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.broker_connection_retry_on_startup = True
 app.autodiscover_tasks()
